86fcde77e1ae4c88302f06e700cbd53d96618853
Gleb Drozdov <glebdrozdov203@gmail.com>
2025-01-06 11:53:07 +0400

    init_commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1>hello veb !!!</h1>
+      </header>
+      <main>
+        <h3>что-то там</h3>
+      </main>
+      
+    </>
+  )
+}
+
+export default App
